44a7e05922f3583ed587ba8019b56433fbdaf078
Zane Jansone <zane.jansone.zj@gmail.com>
2021-03-12 14:31:52 +0200

    Adding Tests

---
+++ src/test/java/com/example/project/controller/FileDataControllerTest.java
@@ -0,0 +1,22 @@
+package com.example.project.controller;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FileDataControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getAll() {
+    }
+}
---
+++ src/test/java/com/example/project/service/FileDataServiceTest.java
@@ -0,0 +1,46 @@
+package com.example.project.service;
+
+import com.example.project.model.FileDataWrap;
+import com.example.project.repository.FileDataRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.io.File;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+import com.example.project.model.FileData;
+
+class FileDataServiceTest {
+
+    private FileDataService fileDataService;
+    private FileDataRepository mockRepo;
+
+    @BeforeEach
+    void setUp() {
+        mockRepo = Mockito.mock(FileDataRepository.class);
+        // need to add repository
+        fileDataService = new FileDataService(mockRepo);
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getAll() {
+        List<FileData> listOfFiles = List.of(
+                FileData.builder().fileName("1").build()
+        );
+        FileDataWrap expected = new FileDataWrap(
+                listOfFiles
+        );
+        when(mockRepo.findAll()).thenReturn(listOfFiles);
+        FileDataWrap result = fileDataService.getAll();
+        assertEquals(expected,result);
+    }
+}
